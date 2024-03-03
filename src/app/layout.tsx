e1805b52d0185b9f0083c4b34fea17d7774ac128
Ko <ko.webcreator@gmail.com>
2024-03-03 18:02:04 +0900

    meta修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import "@/app/globals.scss";
 import { Pacifico } from "next/font/google";
 
@@ -17,6 +17,14 @@ export const metadata: Metadata = {
   twitter: {
     card: "summary_large_image",
   },
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+  viewportFit: "cover",
 };
 
 const pacifico = Pacifico({
