a8824115e9b6c798e1666d13fcf37f111b78ce69
blp490 <blp490@BLP490.local>
2024-05-07 01:50:13 +0900

    2027年4月公開に向けて一旦作業中止

--- src/components/pages/Profile.tsx
+++ src/components/pages/Profile.tsx
@@ -5,8 +5,12 @@ const Profile = () => {
  return (
   <article className={profileStyles.wrap}>
    <h3>WEB CREATOR KO</h3>
+   {/* 2022年4月~2027年4月にスタート */}
+   {/* 描きたい記事
+   ・33歳から未経験で開発エンジニアになり、年収⚪︎⚪︎⚪︎になった話
+   ・30代からの矯正が終わった話 */}
    <p className={profileStyles.des}>
-    33歳から開発未経験で、Web開発に携わり現在 4 年目。
+    33歳から開発未経験で、Web開発に携わり現在 6 年目。
     <br />
     仕事では、Next,jsをフルに活用してフロントからバックエンドの実装を幅広く行なってます。
     <br />
