02df94b293affdee5c060552c732349047c15fd1
blp490 <blp490@BLP490.local>
2024-05-19 13:46:25 +0900

    作品画像追加

--- src/components/pages/works/work.tsx
+++ src/components/pages/works/work.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import workStyles from "./work.module.scss";
+import Image from "next/image";
 const Work = () => {
  return (
   <section className={workStyles.wrap}>
    <h2>YOMcoma</h2>
-   <div className={workStyles.inner}></div>
+   <div className={workStyles.inner}>
+    <div className={workStyles.imageFrame}>
+     <Image src="/yomcoma.png" fill alt="" />
+    </div>
+   </div>
   </section>
  );
 };
