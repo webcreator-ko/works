a6e369efbfe747c6bf2950a9f82e8c396c2b1885
blp490 <blp490@BLP490.local>
2024-05-20 11:05:13 +0900

    works sp対応

--- src/components/pages/works/work.tsx
+++ src/components/pages/works/work.tsx
@@ -4,11 +4,22 @@ import Image from "next/image";
 const Work = () => {
  return (
   <section className={workStyles.wrap}>
-   <h2>YOMcoma</h2>
+   <h2>YOMcoma Web</h2>
    <div className={workStyles.inner}>
     <div className={workStyles.imageFrame}>
      <Image src="/yomcoma.png" fill alt="" />
     </div>
+    <dl>
+     <dt>技術スタック</dt>
+     <dd>
+      Next.js(Pages router), NestJS, MySQL, GCP(主にAppEngine, CloudStorage),
+      外部API(OpenAI, Kyash)
+     </dd>
+     <dt>主な実装</dt>
+     <dd>
+      グループチャット機能、作品画像一括アップロード、AIとOCRを使用した作品タイトルと説明の自動生成機能、Kyashによる送金システム
+     </dd>
+    </dl>
    </div>
   </section>
  );
