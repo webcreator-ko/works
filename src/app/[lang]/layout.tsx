b68cfa91b3d01ae281acc4c0cf59d6b1780bb75f
Ko <ko.webcreator@gmail.com>
2024-08-11 19:11:16 +0900

    next-intl test

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -9,11 +9,16 @@ type Props = {
 
 const langs = ['en', 'ja', 'zh'];
 
+// next-intl 不具合対策用
+// https://github.com/amannn/next-intl/issues/521
+export const dynamic = 'force-dynamic';
+
 export function generateStaticParams() {
   return langs.map((lang) => ({ lang }));
 }
 
 export default async function Layout({ children, params: { lang } }: Props) {
+  console.log('lang', lang);
   unstable_setRequestLocale(lang);
 
   const messages = await getMessages();
