6ecbd998b0e9493bb2574fd19583d1af2a08202e
Ko <ko.webcreator@gmail.com>
2024-08-11 20:08:15 +0900

    ルーティングテスト

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -18,7 +18,7 @@ export function generateStaticParams() {
 }
 
 export default async function Layout({ children, params: { lang } }: Props) {
-  console.log('lang', lang);
+  console.log('lang2', lang);
   unstable_setRequestLocale(lang);
 
   const messages = await getMessages();
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,33 @@
+import { NextRequest, NextResponse } from 'next/server';
 import createMiddleware from 'next-intl/middleware';
 
-export default createMiddleware({
-  // A list of all locales that are supported
+const intlMiddleware = createMiddleware({
   locales: ['en', 'ja', 'zh'],
-
-  // Used when no locale matches
   defaultLocale: 'en',
 });
 
+export function middleware(request: NextRequest) {
+  const geo = request.geo;
+  console.log('geo', geo);
+  let locale = 'en';
+
+  if (geo?.country === 'JP') {
+    locale = 'ja';
+  } else if (geo?.country === 'CN') {
+    locale = 'zh';
+  }
+
+  const pathname = request.nextUrl.pathname;
+
+  // "/" へのアクセス時にロケールに基づいてリダイレクト
+  if (pathname === '/') {
+    return NextResponse.redirect(new URL(`/${locale}`, request.url));
+  }
+
+  // それ以外は intlMiddleware を適用
+  return intlMiddleware(request) || NextResponse.next();
+}
+
 export const config = {
-  // Match only internationalized pathnames
   matcher: ['/', '/(en|ja|zh)/:path*'],
 };
