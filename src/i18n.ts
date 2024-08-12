92e31dfc1ea816043aefca7b0b8428552f4a1e43
Ko <ko.webcreator@gmail.com>
2024-08-12 19:42:58 +0900

    エラー対応

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,11 +1,6 @@
-import { notFound } from 'next/navigation';
 import { getRequestConfig } from 'next-intl/server';
 
-const locales = ['en', 'zh', 'ja'];
-
 export default getRequestConfig(async ({ locale }) => {
-  if (!locales.includes(locale as any)) notFound();
-
   const importData =
     locale == 'ja' || locale == 'zh'
       ? import(`../messages/${locale}.json`)
