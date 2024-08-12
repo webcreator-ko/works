9304a8ae360751a562f4e88648c1c622481d13e3
Ko <ko.webcreator@gmail.com>
2024-08-12 20:01:06 +0900

    エラー対応

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,4 +1,4 @@
-import { Link } from '@/navigation';
+import Link from 'next/link';
 import notFoundStyles from './notFound.module.scss';
 
 const NotFound = () => {
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,37 @@
+import { NextRequest, NextResponse } from 'next/server';
 import createMiddleware from 'next-intl/middleware';
 
-export default createMiddleware({
-  // A list of all locales that are supported
-  locales: ['en', 'zh', 'ja'],
-
-  // Used when no locale matches
+const intlMiddleware = createMiddleware({
+  locales: ['en', 'ja', 'zh'],
   defaultLocale: 'en',
 });
 
+export function middleware(request: NextRequest) {
+  const geo = request.geo;
+  let expectedLocale = 'en';
+
+  if (geo?.country === 'JP') {
+    expectedLocale = 'ja';
+  } else if (geo?.country === 'CN') {
+    expectedLocale = 'zh';
+  }
+
+  const { pathname } = request.nextUrl;
+  // const currentLocale = pathname.split('/')[1];
+
+  // 現在のロケールと期待されるロケールが一致しない場合にリダイレクト
+  // if (currentLocale !== expectedLocale) {
+  //   return NextResponse.redirect(new URL(`/${expectedLocale}`, request.url));
+  // }
+
+  // "/" へのアクセス時にロケールに基づいてリダイレクト
+  if (pathname === '/') {
+    return NextResponse.redirect(new URL(`/${expectedLocale}`, request.url));
+  }
+
+  return intlMiddleware(request) || NextResponse.next();
+}
+
 export const config = {
-  // Match only internationalized pathnames
-  matcher: ['/', '/(en|zh|ja)/:path*'],
+  matcher: ['/', '/(en|ja|zh)/:path*'],
 };
