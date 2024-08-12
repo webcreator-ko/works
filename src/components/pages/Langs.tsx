b44ea7b66290a1d6c6d0d2fad3d28e6380d5dae6
Ko <ko.webcreator@gmail.com>
2024-08-12 14:50:15 +0900

    言語設定の修正

--- src/components/pages/Langs.tsx
+++ src/components/pages/Langs.tsx
@@ -1,21 +1,24 @@
 import Link from 'next/link';
+import { useTranslations } from 'next-intl';
 import langsStyles from './langs.module.scss';
 
 const Langs = () => {
+  const t = useTranslations('Config');
+
   return (
     <ul className={langsStyles.wrap}>
       <li>
-        <Link href="/ja">
+        <Link href="/ja" className={t('lang') === 'ja' ? langsStyles.on : ''}>
           <span className="fi fi-jp"></span>
         </Link>
       </li>
       <li>
-        <Link href="/en">
+        <Link href="/en" className={t('lang') === 'en' ? langsStyles.on : ''}>
           <span className="fi fi-us"></span>
         </Link>
       </li>
       <li>
-        <Link href="/zh">
+        <Link href="/zh" className={t('lang') === 'zh' ? langsStyles.on : ''}>
           <span className="fi fi-cn"></span>
         </Link>
       </li>
