79bdd12e5490960311e38f22eb3bd3e6a70c3658
Ko <ko.webcreator@gmail.com>
2024-08-12 19:56:56 +0900

    エラー対応

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,6 +1,11 @@
+import { notFound } from 'next/navigation';
 import { getRequestConfig } from 'next-intl/server';
 
+const locales = ['en', 'zh', 'ja'];
+
 export default getRequestConfig(async ({ locale }) => {
+  if (!locales.includes(locale as any)) notFound();
+
   const importData =
     locale == 'ja' || locale == 'zh'
       ? import(`../messages/${locale}.json`)
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,37 +1,14 @@
-import { NextRequest, NextResponse } from 'next/server';
 import createMiddleware from 'next-intl/middleware';
 
-const intlMiddleware = createMiddleware({
-  locales: ['en', 'ja', 'zh'],
+export default createMiddleware({
+  // A list of all locales that are supported
+  locales: ['en', 'zh', 'ja'],
+
+  // Used when no locale matches
   defaultLocale: 'en',
 });
 
-export function middleware(request: NextRequest) {
-  const geo = request.geo;
-  let expectedLocale = 'en';
-
-  if (geo?.country === 'JP') {
-    expectedLocale = 'ja';
-  } else if (geo?.country === 'CN') {
-    expectedLocale = 'zh';
-  }
-
-  const { pathname } = request.nextUrl;
-  // const currentLocale = pathname.split('/')[1];
-
-  // 現在のロケールと期待されるロケールが一致しない場合にリダイレクト
-  // if (currentLocale !== expectedLocale) {
-  //   return NextResponse.redirect(new URL(`/${expectedLocale}`, request.url));
-  // }
-
-  // "/" へのアクセス時にロケールに基づいてリダイレクト
-  if (pathname === '/') {
-    return NextResponse.redirect(new URL(`/${expectedLocale}`, request.url));
-  }
-
-  return intlMiddleware(request) || NextResponse.next();
-}
-
 export const config = {
-  matcher: ['/', '/(en|ja|zh)/:path*'],
+  // Match only internationalized pathnames
+  matcher: ['/', '/(en|zh|ja)/:path*'],
 };
